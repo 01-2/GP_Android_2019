7620690b01227b9c3bb00da9721656034562f703
Young-Il Seo <yiseo1011@gmail.com>
2019-07-23 21:32:14 +0900

    Initial state

---
+++ app/src/main/java/com/example/gp_android_2019/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.gp_android_2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button fragBtn = (Button)findViewById(R.id.frag_btn);
+        Button ioBtn = (Button)findViewById(R.id.iocheck_btn);
+        Button benchBtn = (Button)findViewById(R.id.bench_btn);
+
+        fragBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), fragActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        benchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), benchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gp_android_2019/benchActivity.java
@@ -0,0 +1,12 @@
+package com.example.gp_android_2019;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class benchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bench);
+    }
+}
