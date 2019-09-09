edcff58f9db2c696ccc244a27d15c592078a995c
wvvw <yeo8006@gmail.com>
2019-09-09 22:37:51 +0900

    Improve to Display UI

--- app/src/main/java/com/gp_android_2019/bench/DispBenchActivity.java
+++ app/src/main/java/com/gp_android_2019/bench/DispBenchActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.content.Intent;
 import com.gp_android_2019.R;
 
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -28,27 +30,34 @@ public class DispBenchActivity extends AppCompatActivity {
         ParseResultBench parse = new ParseResultBench(results_json);
         results_rw = parse.getPar();
 
-        DispListViewAdapter adapter = new DispListViewAdapter();
+        DispListViewAdapter adapter_rw = new DispListViewAdapter();
         ListView rw_list = (ListView) findViewById(R.id.rw_listView);
-        rw_list.setAdapter(adapter);
+        rw_list.setAdapter(adapter_rw);
 
         for (int i = 0; i < results_rw.size(); i++) {
             String tmp = results_rw.get(i);
             int id = Integer.valueOf(tmp.substring(0, 1));
             String res = tmp.substring(1);
-            adapter.addItem(ttl_rw[id], res);
+            adapter_rw.addItem(ttl_rw[id], res);
         }
+        if (adapter_rw.isEmpty()) {
+            TextView tv = (TextView) findViewById(R.id.rw_ttl);
+            tv.setVisibility(View.GONE);
+        }
+
+        DispListViewAdapter adapter_db = new DispListViewAdapter();
+        ListView db_list = (ListView) findViewById(R.id.db_listView);
+        db_list.setAdapter(adapter_db);
 
         for (int i = 0; i < results_db.size(); i++) {
             String tmp = results_db.get(i);
             int id = Integer.valueOf(tmp.substring(0, 1));
             String res = tmp.substring(1);
-            adapter.addItem(ttl_db[id], res);
+            adapter_db.addItem(ttl_db[id], res);
         }
-
-//        if (adapter.isEmpty()) {
-//            TextView tv = (TextView) findViewById(R.id.rw_ttl);
-//            tv.setVisibility(View.GONE);
-//        }
+        if (adapter_db.isEmpty()) {
+            TextView tv = (TextView) findViewById(R.id.db_ttl);
+            tv.setVisibility(View.GONE);
+        }
     }
 }
